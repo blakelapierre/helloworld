0291da3b0c010bb6f83c46bfd30a0aff8f17985c
Blake La Pierre <blakelapierre@gmail.com>
2014-02-09 18:57:19 -0800

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+	var pkg = grunt.file.readJSON('package.json');
+
+	grunt.initConfig({
+		pkg: pkg,
+		bgShell: {
+			_defaults: {
+				bg: true
+			},
+			startServer: {
+				cmd: 'node server.js',
+				bg: false
+			},
+			startClient: {
+				cmd: 'start http://localhost:3006'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-bg-shell');
+
+	grunt.registerTask('default' , '', function(numberClients) {
+		numberClients = numberClients || 1;
+
+		for (var i = 0; i < numberClients; ++i) {
+			grunt.task.run('bgShell:startClient');
+		}
+		grunt.task.run('bgShell:startServer');
+	});
+};

bde8bf0249252a483f27fac0ef512ec33161e639
Blake La Pierre <blakelapierre@gmail.com>
2014-03-21 04:23:07 -0700

    browserify can't handle this. what I wrote at SMA (now, the opposite of formerly known as VeraCore Software Solutions, Inc.) 4 years ago could.
    if you'd like some reading material while you wait: https://dl.dropboxusercontent.com/u/162863718/SMAComplaint/Complaint.html

--- public/js/facerace/app.js
+++ public/js/facerace/app.js
@@ -1,1 +1,14 @@
-angular.module('facerace', ['angular-gestures', 'faceraceDebug']);
+_.each([
+	'./hud/messageArea.js',
+	'./hud/orientationMetrics.js',
+	'./hud/vehicleInstrumentation.js',
+	'./input/camera.js',
+	'./input/microphone.js',
+	'./input/playerController.js',
+	'./input/userMediaService.js',
+	'./scene/scene.js',
+	'./debug.js',
+	'./facerace.js'
+], require);
+
+module.exports = angular.module('facerace', ['angular-gestures', 'faceraceDebug']);

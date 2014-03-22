10f8961872eae1ac1240722bec7209dcbfcc3832
Blake La Pierre <blakelapierre@gmail.com>
2014-03-21 21:55:12 -0700

    let's try this again

--- public/js/facerace/scene/scene.js
+++
@@ -1,9 +0,0 @@
-angular.module('facerace')
-.directive('scene', function() {
-	return {
-		require: '^facerace',
-		link: function($scope, element, attribute, facerace) {
-			facerace.setClient(faceraceClient('http://' + window.location.host, element, $scope.controls, $scope.config));
-		}
-	};
-});
--- public/js/lib/client.js
+++
@@ -1,9 +0,0 @@
-// Some defaults. Need to clean this up
-
-var lat = 37.8826256,
-	long = -122.269989,
-	debug = true;
-
-var orientation = {current: null};
-
-var map;

4cfb47c34c5c8eec3486ba36b9531992d5c5523e
Blake La Pierre <blakelapierre@gmail.com>
2014-03-26 16:01:23 -0700

    thanks angular, i'll try to take it from here

--- game/public/js/scene/sceneDirective.js
+++ game/public/js/scene/sceneDirective.js
@@ -12,7 +12,7 @@ module.exports = function SceneDirective() {
 				height = window.innerHeight,
 				scene = new THREE.Scene(),
 				renderer = new THREE.WebGLRenderer({antialias: true}),
-				camera = new THREE.PerspectiveCamera(60, width / height, 1, 5000),
+				camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000),
 				stats = new Stats();
 
 			element.append(renderer.domElement);
@@ -22,7 +22,8 @@ module.exports = function SceneDirective() {
 			$scope.renderer = renderer;
 
 
-			camera.up.set(0, 0, 1);
+			camera.up.set(0, 1, 0);
+			camera.position.z = 5;
 
 			scene.add(new THREE.AmbientLight(0xffffff));
 
@@ -31,9 +32,9 @@ module.exports = function SceneDirective() {
 			stats.domElement.style.position = 'absolute';
 			stats.domElement.style.top = '0px';
 
-			var resize = function() {
-				var height = element.innerHeight,
-					width = element.innerWidth;
+			var resize = function(e) {
+				var height = window.innerHeight,
+					width = window.innerWidth;
 					
 				renderer.setSize(width, height);
 				camera.aspect = width / height;
@@ -42,60 +43,63 @@ module.exports = function SceneDirective() {
 
 			window.addEventListener('resize', resize, false);
 
-			$scope.$watchCollection('sources', function(newValue, oldValue, $scope) {
-				var currentKeys = _.keys(newValue),
-					oldKeys = _.keys(oldValue),
+			$scope.liveSources = {};
+			$scope.$watchCollection('sources', function(newValue, ALSONEWVALUEಠ_ಠ, $scope) {
+				var liveSources = $scope.liveSources,
+					currentKeys = _.keys(newValue),
+					oldKeys = _.keys(liveSources),
 					newKeys = _.difference(currentKeys, oldKeys),
 					removableKeys = _.difference(oldKeys, currentKeys);
 
-				console.log(currentKeys, oldKeys, newKeys, removableKeys);	
-			});
+				_.each(newKeys, function(newKey) {
+					console.log(newKey);
+					var videoSource = newValue[newKey],
+						video = videoSource.element,
+						width = 10,
+						height = 10,
+						texture = new THREE.Texture(video), 
+						material = new THREE.ShaderMaterial({
+							fragmentShader: document.getElementById('plane-fragment-shader-swirl').textContent,
+							vertexShader: document.getElementById('plane-vertex-shader-swirl').textContent,
+							uniforms: {
+								texture: {type: 't', value: texture},
+								width: {type: 'f', value: width},
+								height: {type: 'f', value: height}
+							},
+							side: THREE.DoubleSide
+						}),
+						mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material);
+						
+					texture.anisotropy = renderer.getMaxAnisotropy();
 
-			// $scope.$watchCollection('videoSources', function(currentPeers, oldPeers) {
-			// 	var checked = {};
+					scene.add(mesh);
 
-			// 	_.each(currentPeers, function(peer) {
-			// 		if (peer.object3D == null) {
-			// 			var width = 1,
-			// 				height = 1,
-			// 				peerVideo = document.getElementById(peer.elementID),
-			// 				texture = new THREE.Texture(peerVideo), 
-			// 				material = new THREE.ShaderMaterial({
-			// 					fragmentShader: document.getElementById('plane-fragment-shader').textContent,
-			// 					vertexShader: document.getElementById('plane-vertex-shader').textContent,
-			// 					uniforms: {
-			// 						texture: {type: 't', value: texture},
-			// 						width: {type: 'f', value: width},
-			// 						height: {type: 'f', value: height}
-			// 					},
-			// 					side: THREE.DoubleSide
-			// 				}),
-			// 				mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material);
-							
-			// 			texture.anisotropy = renderer.getMaxAnisotropy();
+					videoSource.mesh = mesh;
+					videoSource.texture = texture;
+					liveSources[newKey] = videoSource;
+				});
 
-			// 			mesh.position.z = -10;
-			// 			scene.add(mesh);
-			// 			camera.lookAt(mesh);
+				_.each(removableKeys, function(removableKey) {
 
-			// 			peer.object3D = {
-			// 				peerVideo: peerVideo,
-			// 				texture: texture,
-			// 				mesh: mesh
-			// 			};
-			// 		}
-			// 	});
-			// });
+				});
+			}, true);
 
 			var render = function() {
-				_.each($scope.peers, function(peer) {
-					if (peer.object3D && peer.object3D.peerVideo.readyState == peer.object3D.peerVideo.HAVE_ENOUGH_DATA) {
-						peer.object3D.texture.needsUpdate = true;
+				window.requestAnimationFrame(render);
+
+				var source = $scope.liveSources['local'];
+				if (source && source.mesh) camera.lookAt(source.mesh.position);
+
+				_.each(_.pairs($scope.liveSources), function(pair) {
+					var source = pair[1],
+						element = source.element;
+					if (element.readyState == element.HAVE_ENOUGH_DATA) {
+						source.texture.needsUpdate = true;
 					}
 				});
+
 				renderer.render(scene, camera);
 				stats.update();
-				window.requestAnimationFrame(render);
 			};
 			window.requestAnimationFrame(render);
 		}

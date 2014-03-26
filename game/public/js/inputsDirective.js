ec54f95f3f49c51947880b871813b546e2a8d7e4
Blake La Pierre <blakelapierre@gmail.com>
2014-03-26 14:04:29 -0700

    'nuff said

--- game/public/js/inputsDirective.js
+++ game/public/js/inputsDirective.js
@@ -5,40 +5,43 @@ module.exports = function CameraDirective($sce) {
 	return {
 		restrict: 'E',
 		link: function($scope, element, attributes) {
-			var createVideo = function(stream) {
+			var createVideo = function(stream, socketID) {
 				var video = document.createElement('video');
 				video.src = $sce.trustAsResourceUrl(URL.createObjectURL(stream));
 				video.autoplay = true;
 
 				element.append(video);
+
+				return {
+					element: video,
+					src: video.src,
+					stream: stream,
+					socketID: socketID || 'local'
+				};
 			};
+
 			$scope.videoSources = [];
 
 			rtc.createStream({video: true, audio: true}, function(stream) {
-				createVideo(stream);
+				$scope.videoSources.push(createVideo(stream));
 			});
 
 			rtc.connect('ws://' + window.location.hostname + ':3007', 'facerace');
 
 			rtc.on('add remote stream', function(stream, socketID) {
-				var peer = {
-					elementID: 'peer-video-' + socketID,
-					socketID: socketID,
-					streamSource: $sce.trustAsResourceUrl(URL.createObjectURL(stream))
-				};
-				createVideo(stream, socketID);
+				$scope.videoSources.push(createVideo(stream, socketID));
 			});
 
 			rtc.on('disconnect stream', function(socketID) {
 				var index = 0;
-				for (var i = 0; i < $scope.peers.length; i++) {
-					if ($scope.peers.socketID == socketID) {
-						index = i;
-						break;
+				for (var i = 0; i < $scope.videoSources.length; i++) {
+					var source = $scope.videoSources[i];
+					if (source.socketID == socketID) {
+						source.video.remove();
+						$scope.videoSources.splice(i, 1);
+						break; // get out of here now! :)
 					}
 				}
-				$scope.peers.splice(index, 1);
-				$scope.$apply();
 			});
 		}
 	};	
